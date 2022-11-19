76c89e9b843cbd7c31da6738e0bb38c15910fddb
011011000110110001110000 <106524046011011000110110001110000@users.noreply.github.com>
2022-11-19 19:33:16 +0100

    Removed unneeded Redirect and minor cleanup, bump version to 1.0.1

--- src/main/java/thread/safe/beehives/ThreadSafeBeehives.java
+++ src/main/java/thread/safe/beehives/ThreadSafeBeehives.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 public class ThreadSafeBeehives implements ModInitializer {
     public static final String MOD_ID = "thread-safe-beehives";
     public static final String MOD_NAME = "Thread Safe Beehives";
-    public static final String MOD_VERSION = "1.0.0";
+    public static final String MOD_VERSION = "1.0.1";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
     @Override
     public void onInitialize() {
